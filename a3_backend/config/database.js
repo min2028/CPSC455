374c5e44b7b1c0685a17bfbfc6d4ed097d3e3169
min2028 <ronaldaungkhantmin@gmail.com>
2023-07-22 18:08:27 -0700

    Added mongo atlas db and changed configs

--- a3_backend/config/database.js
+++ a3_backend/config/database.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-const MONGODB_URI = "mongodb://localhost:27017/Inventory";
+const MONGODB_URI = process.env.MONGODB_URI;
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log('Connected to the database');
