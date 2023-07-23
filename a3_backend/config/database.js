8ca653a2a118d2c7578e50207939c4be0fc99347
min2028 <ronaldaungkhantmin@gmail.com>
2023-07-22 18:33:16 -0700

    change env

--- a3_backend/config/database.js
+++ a3_backend/config/database.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const MONGODB_URI = process.env.MONGODB_URI;
+//"mongodb://localhost:27017/Inventory"
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log('Connected to the database');
